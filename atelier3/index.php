c52795de6982149b0f5c563128ef3db66ddcdb2e
Nolan Guiziou <guiziou.nolan@gmail.com>
2024-12-18 13:37:16 +0100

    index

--- atelier3/index.php
+++ atelier3/index.php
@@ -2,36 +2,31 @@
 // Démarre la session
 session_start();
 
-// Vérifier si l'utilisateur est déjà connecté
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    if ($_SESSION['username'] === 'admin') {
+    if ($_SESSION['user_type'] === 'admin') {
         header('Location: page_admin.php');
-    } elseif ($_SESSION['username'] === 'user') {
+    } else {
         header('Location: page_user.php');
     }
     exit();
 }
 
-// Gérer le formulaire de connexion
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Vérification des identifiants
     if ($username === 'admin' && $password === 'secret') {
-        // Stocker les informations utilisateur dans la session
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
+        $_SESSION['user_type'] = 'admin';
 
-        // Rediriger vers la page admin
         header('Location: page_admin.php');
         exit();
     } elseif ($username === 'user' && $password === 'utilisateur') {
-        // Stocker les informations utilisateur dans la session
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
+        $_SESSION['user_type'] = 'user';
 
-        // Rediriger vers la page user
         header('Location: page_user.php');
         exit();
     } else {
