7a9746b0fced88268868514dfa5ecfcfaabf2d74
Nolan Guiziou <guiziou.nolan@gmail.com>
2024-12-18 10:58:15 +0100

    cookie random

--- atelier2/index.php
+++ atelier2/index.php
@@ -18,7 +18,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', '12345', time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        $authToken = bin2hex(random_bytes(16));
+        $_SESSION['authToken'] = $authToken; // Coté serv
+        setcookie('authToken', $authToken, time() + 60, '/', '', false, true); // Cookie valable 1 minute (60 secondes)
+
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
